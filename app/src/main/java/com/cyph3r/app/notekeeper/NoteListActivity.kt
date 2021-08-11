e780142dc23403445b0b0304ac826b73ad164dd4
Tade Ajai <tadeajai4@gmail.com>
2021-08-11 18:57:36 +0100

    Fixed FAB rendering problems
    Signed-off-by: Tade Ajai <tadeajai4@gmail.com>

--- app/src/main/java/com/cyph3r/app/notekeeper/NoteListActivity.kt
+++ app/src/main/java/com/cyph3r/app/notekeeper/NoteListActivity.kt
@@ -2,32 +2,41 @@ package com.cyph3r.app.notekeeper
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 import kotlinx.android.synthetic.main.activity_list_note.*
+import kotlinx.android.synthetic.main.drawer.*
 
-class NoteListActivity : AppCompatActivity() {
+
+
+class NoteListActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_note)
-//        setSupportActionBar(toolbar_list_note)
+        setContentView(R.layout.drawer)
+//        setSupportActionBar(toolbar_note_list)
 
         add_note_button.setOnClickListener { _ ->
-            val activityIntent = Intent(this, DrawerMenu::class.java)
+            val activityIntent = Intent(this, EditNoteActivity::class.java)
             activityIntent.putExtra(NOTE_POSITION, EXTRA_NO_NOTE_POSITION)
             startActivity(activityIntent)
         }
-        list_item.layoutManager = LinearLayoutManager(this)
+        note_course_list.layoutManager = LinearLayoutManager(this)
 //        list_item.layoutManager.
-        list_item.adapter = NoteRecyclerAdapter(this, DataManager.notes)
+        note_course_list.adapter = NoteRecyclerAdapter(this, DataManager.notes)
 
     }
 
     override fun onResume() {
         super.onResume()
-        list_item.adapter.notifyDataSetChanged()
+        note_course_list.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 
 
